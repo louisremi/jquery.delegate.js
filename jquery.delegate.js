ef956218d7a9775a8d741d1bef0d9a893ef19815
lrbabe <lrbabe@lrbabe-laptop>
2009-11-10 22:41:53 +0100

    Use internal jQuery.event methods + use jQuery plugin pattern + cache global variables

--- jquery.delegate.js
+++ jquery.delegate.js
@@ -1,56 +1,64 @@
 (function($) {
 
 $.fn.delegate = function(type, selector, callback) {
-	switch(type) {
-		case "mouseenter":
-		case "mouseleave":
-			return $(this).bind(type, function( event ) {
-				var $elem = $(event.target).closest(selector),
-					$related = $(event.relatedTarget).closest(selector, this); 
-				if(!$related.length || $related[0] != $elem[0]) {
-					event.currentTarget = $elem[0]; 
-					return callback.apply(this, arguments);
-				}
-			});
-			break;
-		case "submit":
-		case "change":
-		case "select":
-			// Use co-opted focusin/out event for IE
-			if( typeof document.attachEvent == "object" ) {
-				return $(this).bind("focusin."+type+"Focus", function( event ) {
-					var $elem = $(event.target).closest(selector, this),
-						args = arguments,
-						self = this;
-					if($elem.length) {
-						event.currentTarget = $elem[0];
-						$(event.target).bind(type+"."+type+"Focus", function() {
-							callback.apply(self, args);
-						});
-					}
-				}).bind("focusout."+type+"Focus", function( event ) {
-					var $elem = $(event.target).closest(selector, this); 
-					if($elem.length) {
-						$(event.target).unbind(type, callback);
+	return this.each(function() {
+		var $event = $.event,
+			$add = $event.add;
+		
+		switch(type) {
+			case "mouseenter":
+			case "mouseleave":
+				$add(this, type, function( event ) {
+					var $elem = $(event.target).closest(selector),
+						$related = $(event.relatedTarget).closest(selector, this); 
+					if(!$related.length || $related[0] != $elem[0]) {
+						event.currentTarget = $elem[0]; 
+						return callback.apply(this, arguments);
 					}
 				});
 				break;
-			}
-			// those events do bubble in other browsers
-		default:
-			return $(this).bind(type, function( event ) {
-				var $elem = $(event.target).closest(selector, this);
-				if($elem.length) {
-					event.currentTarget = $elem[0];
-					return callback.apply(this, arguments);
+			case "submit":
+			case "change":
+			case "select":
+				// Use co-opted focusin/out event for IE
+				if( typeof document.attachEvent == "object" ) {
+					$add(this, "focusin."+type+"Focus", function( event ) {
+						var $elem = $(event.target).closest(selector, this),
+							args = arguments,
+							self = this;
+						if($elem.length) {
+							event.currentTarget = $elem[0];
+							$add(event.target, type+"."+type+"Focus", function() {
+								callback.apply(self, args);
+							});
+						}
+					});
+					$add(this, "focusout."+type+"Focus", function( event ) {
+						var $elem = $(event.target).closest(selector, this); 
+						if($elem.length) {
+							$event.remove(event.target, type, callback);
+						}
+					});
+					break;
+				// those events do bubble in other browsers
 				}
-			});
-			break;
-	}
+			default:
+				$add(this, type, function( event ) {
+					var $elem = $(event.target).closest(selector, this);
+					if($elem.length) {
+						event.currentTarget = $elem[0];
+						return callback.apply(this, arguments);
+					}
+				});
+				break;
+		}
+	});
 }
 
 $.fn.undelegate = function(type, callback) {
-	return $(this).unbind(type.replace(/(submit|change|select)/, ".$1Focus"), callback);
+	return this.each(function() {
+		$.event.remove(this, type.replace(/(submit|change|select)/, ".$1Focus"), callback);
+	});
 };
 
 })(jQuery);
